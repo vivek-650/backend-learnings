bc99ba8bd7e8b3ab0f55801a6df7529c91894192
Vivek <vivek66anand66@gmail.com>
2025-10-16 02:04:19 +0530

    add subscription model and refresh token route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import upload from "../middleware/multer.middleware.js";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, resetAccessTokens } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = Router();
 
@@ -18,5 +18,5 @@ router.route("/login").post(loginUser);
 
 // secured routes can be added here using verifyJWT middleware
 router.route("/logout").post(verifyJWT, logoutUser);
-
+router.route("/refresh-token").post(resetAccessTokens);
 export default router;
